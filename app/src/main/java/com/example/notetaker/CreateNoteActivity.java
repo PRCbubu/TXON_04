4c7fe4780e9bdcc13bde82b1e74c8d730b66932b
BabuRoyC <prcbubu@gmail.com>
2023-05-04 00:50:28 +0530

    Made the second activity_create_note.xml for taking notes

---
+++ app/src/main/java/com/example/notetaker/CreateNoteActivity.java
@@ -0,0 +1,16 @@
+package com.example.notetaker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateNoteActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_note);
+    }
+}
